2673f2b35eb89afd97c45730a188f522e6bf8850
ganges <akapikumin100piki@gmail.com>
2021-10-08 22:33:20 +0900

    Firestoreへデータを格納するテストをした

--- app/src/main/java/com/example/driver_app/MainActivity.kt
+++ app/src/main/java/com/example/driver_app/MainActivity.kt
@@ -7,9 +7,18 @@ import com.google.firebase.firestore.ktx.firestore
 
 val db = Firebase.firestore
 
+// Create a new user with a first and last name
+val user = hashMapOf(
+    "first" to "Ada",
+    "last" to "Lovelace",
+    "born" to 1815
+)
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        db.collection("users").add(user)
     }
 }
