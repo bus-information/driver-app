dedd8d699a85633dd1110b7ac08b48af4eb09890
ganges <akapikumin100piki@gmail.com>
2021-10-08 19:17:33 +0900

    firestoreの環境設定をした

--- app/src/main/java/com/example/driver_app/MainActivity.kt
+++ app/src/main/java/com/example/driver_app/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.driver_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.firestore.ktx.firestore
+
+val db = Firebase.firestore
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
