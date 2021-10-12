9431717aff8a9932555212292ff4a2e5dcdcb1a4
rikuworks <mtmc.blade@gmail.com>
2021-10-12 11:19:27 +0900

    fic

--- app/src/main/java/com/example/driver_app/MainActivity.kt
+++ app/src/main/java/com/example/driver_app/MainActivity.kt
@@ -9,6 +9,12 @@ import android.os.Bundle
 import androidx.core.content.ContextCompat
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.firestore.ktx.firestore
+import android.Manifest
+import android.content.pm.PackageManager
+import android.widget.Button
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.*
 
 val db = Firebase.firestore
 
@@ -20,10 +26,81 @@ val user = hashMapOf(
 )
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var callback: LocationCallback
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        db.collection("users").add(user)
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
+        val getButton:Button = findViewById(R.id.get_button)
+        getButton.setOnClickListener {
+            fetchLatestLocation()
+        }
+
+        callback = object : LocationCallback() {
+            override fun onLocationResult(p0: LocationResult) {
+                super.onLocationResult(p0)
+            }
+        }
     }
+
+    private fun fetchLatestLocation() {
+        var latestLocation = fusedLocationProviderClient.lastLocation
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ),
+                101
+            )
+        } else if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ),
+                101
+            )
+        } else if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ),
+                101
+            )
+        } else {
+            latestLocation.addOnSuccessListener {
+                if (it != null) {
+                    Toast.makeText(this, "${it.latitude} \n ${it.longitude}", Toast.LENGTH_SHORT).show()
+                }
+                val locationRequest = createLocationRequest()?:return@addOnSuccessListener
+                fusedLocationProviderClient.requestLocationUpdates(
+                    locationRequest,
+                    callback,
+                    null
+                )
+            }
+        }
+    }
+
+    private fun createLocationRequest(): LocationRequest {
+        return LocationRequest.create().apply {
+            interval = 10000
+            fastestInterval = 5000
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+    }
+
 }
+
