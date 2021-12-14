264aac75b48aa161e79a2e2e26cf6c566963b3aa
rikuworks <mtmc.blade@gmail.com>
2021-12-14 10:16:09 +0900

    aaaa

--- app/src/main/java/com/example/driver_app/MainActivity.kt
+++ app/src/main/java/com/example/driver_app/MainActivity.kt
@@ -27,10 +27,17 @@ import com.google.android.gms.maps.model.MarkerOptions
 
 @SuppressLint("StaticFieldLeak")
 val db = Firebase.firestore
-class MainActivity : AppCompatActivity(),OnMapReadyCallback  {
+
+var aId = "0"
+var bLocate = "0"
+var tStamp = "0"
+var sId = "0"
+val sList = arrayOf("")
+
+class MainActivity : AppCompatActivity(),OnMapReadyCallback {
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     private lateinit var callback: LocationCallback
-    var a=0
+    var a = 0
     private val handler = Handler()
     private lateinit var mMap: GoogleMap
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -52,13 +59,12 @@ class MainActivity : AppCompatActivity(),OnMapReadyCallback  {
             x++
         }
 
-        getButton.setOnCheckedChangeListener  { _, isChecked ->
+        getButton.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
-                a=1
+                a = 1
                 Timer().schedule(0, 1000, timerCallback1)
-            }
-            else if(!isChecked){
-                a=0
+            } else if (!isChecked) {
+                a = 0
             }
         }
 
@@ -68,6 +74,7 @@ class MainActivity : AppCompatActivity(),OnMapReadyCallback  {
             }
         }
     }
+
     override fun onMapReady(googleMap: GoogleMap?) {
         if (googleMap != null) {
             mMap = googleMap
@@ -93,9 +100,9 @@ class MainActivity : AppCompatActivity(),OnMapReadyCallback  {
         ll.addOnSuccessListener {
             var now_pojit = LatLng(it.latitude, it.longitude)
             mMap.addMarker(MarkerOptions().position(now_pojit).title("現在地"))
-            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(now_pojit,14f))
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(now_pojit, 14f))
         }
-        val locationRequest = createLocationRequest()?:return
+        val locationRequest = createLocationRequest() ?: return
         fusedLocationProviderClient.requestLocationUpdates(
             locationRequest,
             callback,
@@ -105,71 +112,103 @@ class MainActivity : AppCompatActivity(),OnMapReadyCallback  {
 
 
     private fun fetchLatestLocation() {
-            val latestLocation = fusedLocationProviderClient.lastLocation
+        val latestLocation = fusedLocationProviderClient.lastLocation
 
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)!= PackageManager.PERMISSION_GRANTED)
-            {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
 
-                ActivityCompat.requestPermissions(
-                    this,
-                    arrayOf(
-                        Manifest.permission.ACCESS_COARSE_LOCATION,
-                        Manifest.permission.ACCESS_FINE_LOCATION
-                    ),
-                    101
-                )
-            }
-            else if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
-            {
-                ActivityCompat.requestPermissions(
-                    this,
-                    arrayOf(
-                        Manifest.permission.ACCESS_COARSE_LOCATION
-                    ),
-                    101
-                )
-            }
-            else if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
-            {
-                ActivityCompat.requestPermissions(
-                    this,
-                    arrayOf(
-                        Manifest.permission.ACCESS_FINE_LOCATION
-                    ),
-                    101
-                )
-            }
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ),
+                101
+            )
+        } else if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ),
+                101
+            )
+        } else if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ),
+                101
+            )
+        } else {
+            latestLocation.addOnSuccessListener {
 
-            else {
-                latestLocation.addOnSuccessListener {
-
-                    if (a==1) {
-                        if (it != null) {
-                            Toast.makeText(this, "${it.latitude} \n ${it.longitude}", Toast.LENGTH_SHORT).show()
-                            val radioGroup = findViewById<RadioGroup>(R.id.RadioGroup)
-                            val id = radioGroup.checkedRadioButtonId
-                            val checkedRadioButton = findViewById<RadioButton>(id)
-                            val user = hashMapOf(
-                                "latitude" to "${it.latitude}",
-                                "longitude" to "${it.longitude}",
-                                "going" to checkedRadioButton.text
-                            )
-                            println(checkedRadioButton.text)
-                            // Add a new document with a generated ID
-                            db.collection("users")
-                                .add(user)
-                                .addOnSuccessListener { documentReference ->
-                                    Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+                if (a == 1) {
+                    if (it != null) {
+                        Toast.makeText(
+                            this,
+                            "${it.latitude} \n ${it.longitude}",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        val radioGroup = findViewById<RadioGroup>(R.id.RadioGroup)
+                        val id = radioGroup.checkedRadioButtonId
+                        val checkedRadioButton = findViewById<RadioButton>(id)
+                        val user = hashMapOf(
+                            "latitude" to "${it.latitude}",
+                            "longitude" to "${it.longitude}",
+                            "going" to checkedRadioButton.text
+                        )
+                        println(checkedRadioButton.text)
+                        // Add a new document with a generated ID
+                        db.collection("bus")
+                            .document("gpucZzxPYDlp5sWXhQOO")
+                            .get()
+                            .addOnCompleteListener { task ->
+                                if (task.isSuccessful) {
+                                    val document = task.result
+                                    if (document != null && document.data != null) {
+                                        aId = document.data?.get("arrivalTimeId").toString()
+                                        bLocate = document.data?.get("locate").toString()
+                                        sId = document.data?.get("stationsId").toString()
+                                        tStamp = document.data?.get("timeStamp").toString()
+                                    }
                                 }
-                                .addOnFailureListener { e ->
-                                    Log.w(TAG, "Error adding document", e)
-                                }
-                        }
-                    }
-                    else if(a==0){
+
+                                db.collection(sId)
+                                    .get()
+                                    .addOnSuccessListener { result ->
+                                        for (document in result) {
+                                            Log.d(TAG, "${document.id} => ${document.data}")
+                                        }
+
+                                    }
+                                    .addOnFailureListener { exception ->
+                                        Log.w(TAG, "No of Station...", exception)
+                                    }
+                            }
+                    } else if (a == 0) {
                         println("fuck")
                     }
-                    val locationRequest = createLocationRequest()?:return@addOnSuccessListener
+                    println(aId)
+                    println(bLocate)
+                    println(sId)
+                    println(tStamp)
+                    val locationRequest = createLocationRequest() ?: return@addOnSuccessListener
                     fusedLocationProviderClient.requestLocationUpdates(
                         locationRequest,
                         callback,
@@ -178,8 +217,8 @@ class MainActivity : AppCompatActivity(),OnMapReadyCallback  {
                 }
             }
 
+        }
     }
-
     private fun createLocationRequest(): LocationRequest? {
         return LocationRequest.create().apply {
             interval = 10000
@@ -187,7 +226,4 @@ class MainActivity : AppCompatActivity(),OnMapReadyCallback  {
             priority = LocationRequest.PRIORITY_HIGH_ACCURACY
         }
     }
-
-
 }
-
